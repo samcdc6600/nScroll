fab2f25f2a2e569a08d09006cff3899ccc708c99
SB <s3545852@student.rmit.edu.au>
2019-02-15 02:56:07 +1100

    Changed all files with the .cpp extension to use the .c++ extension to alighn better with the change from using .h to .h++

--- working/build.sh
+++ working/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # The plan is to replace this with a make file eventually when I learn how to actually make one that's of any use :'(
-clang++ -pedantic-errors -Wall -std=c++14 *.cpp ./draw/*.cpp ./initial/checkBoundsOfBounds/*.cpp ./initial/collapse/*.cpp ./initial/load/*.cpp ./physics/*.cpp ./sprite/player/*.cpp ./sprite/*.cpp -lncurses
-# g++ -pedantic-errors -Wall -std=c++14 *.cpp ./draw/*.cpp ./initial/checkBoundsOfBounds/*.cpp ./initial/collapse/*.cpp ./initial/load/*.cpp ./physics/*.cpp ./sprite/player/*.cpp ./sprite/*.cpp -lncurses
+clang++ -pedantic-errors -Wall -std=c++14 *.c++ ./draw/*.c++ ./initial/checkBoundsOfBounds/*.c++ ./initial/collapse/*.c++ ./initial/load/*.c++ ./physics/*.c++ ./sprite/player/*.c++ ./sprite/*.c++ -lncurses
+# g++ -pedantic-errors -Wall -std=c++14 *.c++ ./draw/*.c++ ./initial/checkBoundsOfBounds/*.c++ ./initial/collapse/*.c++ ./initial/load/*.c++ ./physics/*.c++ ./sprite/player/*.c++ ./sprite/*.c++ -lncurses
 # remove crud >:(
 rm    ./*.gch ./*~ ./*#    ./draw/*.gch ./draw/*~ ./draw/*#    ./initial/checkBoundsOfBounds/*.gch ./initial/checkBoundsOfBounds/*~ ./initial/checkBoundsOfBounds/*#    ./initial/collapse/*.gch ./initial/collapse/*~ ./initial/collapse/*#    ./initial/load/*.gch ./initial/load/*~ ./initial/load/*#    ./physics/*.gch ./physics/*~ ./physics/*#    ./sprite/player/*.gch ./sprite/player/*~ ./sprite/player/*#     ./sprite/*.gch ./sprite/*~ ./sprite/*#         ./assets/sprites/*# ./assets/sprites/*~    ./assets/sprites/*# ./assets/sprites/*~
